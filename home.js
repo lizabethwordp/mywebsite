2b907ea101506bbabd82ae434f09e485927eee55
unknown <you@example.com>
2022-07-13 16:53:51 +0100

    my web commit

---
+++ home.js
@@ -0,0 +1,14 @@
+window.onload = function () {
+  var nav = document.getElementById("navbar");
+  //var item = document.getElementsByTagName("li");
+//   var list = document.getElementById("nav-list");
+
+  nav.style.width = "100%";
+  nav.style.backgroundColor = "grey";
+
+  
+  for (var i = 0; i < item.length; i++) {
+    item[i].style.display = "inline-block";
+    item[i].style.padding = "10px";
+  }
+}
